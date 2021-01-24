72fbd63df2b7778e9b00f7a5f191a51517cce1f6
Carmine Watcher <CWatcher@student.21-school.ru>
2021-01-24 11:49:39 +0300

    + header

--- main.c
+++ main.c
@@ -8,8 +8,10 @@ int	 main(int ac, char *av[]) {
 	int (*print)(const char *, ...);
 	#ifdef LIBC
 		print = printf;
+		print("================LibC================\n");
 	#else
 		print = ft_printf;
+		print("================LibFT===============\n");
 	#endif
 	print("ab%s%s%si\n", "cde", "fg", "h");
 }
