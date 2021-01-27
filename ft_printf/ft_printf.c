6cec5abb1e9625255a958b43e956003bb7e59aca
Carmine Watcher <cwatcher@21-school.ru>
2021-01-27 16:41:51 +0300

    [buggy][bugfix][squash] width & prec

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -26,10 +26,10 @@ static ssize_t	prn_frmt(char const **ps, va_list *pap)
 		if (frmt.prec < 0)
 			frmt.prec = ft_strlen(s);
 		if (!frmt.left)
-			n += ft_putcn(frmt.padd, frmt.wdth - (ft_strlen(s) - frmt.prec));
+			n += ft_putcn(frmt.padd, frmt.wdth - frmt.prec);
 		n += ft_putsn(s, frmt.prec);
 		if (frmt.left)
-			n += ft_putcn(frmt.padd, frmt.wdth - (ft_strlen(s) - frmt.prec));
+			n += ft_putcn(frmt.padd, frmt.wdth - frmt.prec);
 	}
 	else if (ft_strchr("di", **ps))
 		n += print_d(&frmt, va_arg(*pap, int));
