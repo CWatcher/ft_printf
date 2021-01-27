430ea5c436ed591e972929c29d2a27bed355bf30
Carmine Watcher <cwatcher@21-school.ru>
2021-01-27 18:43:34 +0300

    [squash] const

--- ft_printf/libft/ft_puts.c
+++ ft_printf/libft/ft_puts.c
@@ -6,14 +6,14 @@
 /*   By: CWatcher <cwatcher@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 11:45:31 by CWatcher          #+#    #+#             */
-/*   Updated: 2021/01/25 13:18:22 by CWatcher         ###   ########.fr       */
+/*   Updated: 2021/01/27 11:49:36 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-ssize_t	ft_puts(char *s)
+ssize_t	ft_puts(const char *s)
 {
 	return (write(1, s, ft_strlen(s) * sizeof(*s)));
 }
--- ft_printf/libft/ft_putsn.c
+++ ft_printf/libft/ft_putsn.c
@@ -6,14 +6,14 @@
 /*   By: CWatcher <cwatcher@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 19:11:54 by CWatcher          #+#    #+#             */
-/*   Updated: 2021/01/26 19:19:48 by CWatcher         ###   ########.fr       */
+/*   Updated: 2021/01/27 11:50:29 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-ssize_t	ft_putsn(char *s, ssize_t n)
+ssize_t	ft_putsn(const char *s, ssize_t n)
 {
 	const ssize_t	l = ft_strlen(s);
 
